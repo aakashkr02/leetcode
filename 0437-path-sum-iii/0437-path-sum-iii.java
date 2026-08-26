91537454a7ed5a222968cfb322b3770f3d7ef944
Aakash Kumar <aakash112004kr@gmail.com>
2026-08-27 00:41:09 +0530

    Time: 18 ms (40.9%), Space: 45.2 MB (60.1%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        return countPaths(root, targetSum)
+             + pathSum(root.left, targetSum)
+             + pathSum(root.right, targetSum);
+    }
+
+    private int countPaths(TreeNode node, long targetSum) {
+        if (node == null) {
+            return 0;
+        }
+
+        int count = 0;
+
+        if (node.val == targetSum) {
+            count++;
+        }
+
+        count += countPaths(node.left, targetSum - node.val);
+        count += countPaths(node.right, targetSum - node.val);
+
+        return count;
+    }
+}
